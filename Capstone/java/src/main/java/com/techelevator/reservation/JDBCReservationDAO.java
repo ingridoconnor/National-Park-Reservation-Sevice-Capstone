4cbdb46c898f34533955f1e528da0d7eec104e13
Ingrid OConnor <ingrido@bgsu.edu>
2020-06-18 17:37:30 -0400

    reservation id

--- Capstone/java/src/main/java/com/techelevator/reservation/JDBCReservationDAO.java
+++ Capstone/java/src/main/java/com/techelevator/reservation/JDBCReservationDAO.java
@@ -35,3 +35,30 @@ public class JDBCReservationDAO implements ReservationDAO {
 	
 
 
+@Override
+public Reservation createReservation(Reservation reservationId) {
+	String insertNewReservation = "INSERT INTO reservation (name, from_date, to_date) VALUES (?, ?, ?,) RETURNING reservation_id";
+	Long newResId = jdbcTemplate.queryForObject(insertNewReservation, Long.class, reservationId.getReservationId());
+	reservationId.setReservationId(newResId);
+	return reservationId;
+}
+
+
+private long getReservationId(Reservation res) {
+	return res.getReservationId();
+}
+
+private Reservation mapRowToReservation(SqlRowSet results) {
+	Reservation reservation = new Reservation();
+	reservation.setCreateDate(results.getDate("create_date").toLocalDate());
+	reservation.setFromDate(results.getDate("from_date").toLocalDate());
+	reservation.setToDate(results.getDate("to_date").toLocalDate());
+	reservation.setSiteId(results.getLong("site_id"));
+	reservation.setReservationId(results.getLong("reservation_id"));
+	reservation.setName(results.getString("name"));
+	return reservation;
+}	
+	
+	
+	
+}
