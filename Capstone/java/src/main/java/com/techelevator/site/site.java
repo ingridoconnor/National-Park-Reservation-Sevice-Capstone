cbe3c673b3ad6ecfdff65d880ea9d78326d80540
Jesse <jesse.l.heller@gmail.com>
2020-06-18 13:26:58 -0400

    Added site & reservation classes

---
+++ Capstone/java/src/main/java/com/techelevator/site/site.java
@@ -0,0 +1,68 @@
+package com.techelevator.site;
+
+public class site {
+
+    private Long siteId;
+    private Long campgroundId;
+    private int siteNumber;
+    private int maxOccupancy;
+    private boolean accessible;
+    private int maxRvLength;
+    private boolean utilities;
+
+    public Long getSiteID() {
+        return siteId;
+    }
+
+    public void setSiteID(Long siteID) {
+        this.siteId = siteID;
+    }
+
+    public Long getCampgroundId() {
+        return campgroundId;
+    }
+
+    public void setCampgroundId(Long campgroundId) {
+        this.campgroundId = campgroundId;
+    }
+
+    public int getSiteNumber() {
+        return siteNumber;
+    }
+
+    public void setSiteNumber(int siteNumber) {
+        this.siteNumber = siteNumber;
+    }
+
+    public int getMaxOccupancy() {
+        return maxOccupancy;
+    }
+
+    public void setMaxOccupancy(int maxOccupancy) {
+        this.maxOccupancy = maxOccupancy;
+    }
+
+    public boolean isAccessible() {
+        return accessible;
+    }
+
+    public void setAccessible(boolean accessible) {
+        this.accessible = accessible;
+    }
+
+    public int getMaxRvLength() {
+        return maxRvLength;
+    }
+
+    public void setMaxRvLength(int maxRvLength) {
+        this.maxRvLength = maxRvLength;
+    }
+
+    public boolean isUtilities() {
+        return utilities;
+    }
+
+    public void setUtilities(boolean utilities) {
+        this.utilities = utilities;
+    }
+}
