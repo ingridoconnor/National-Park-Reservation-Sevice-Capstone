2d273ffff4f7b0a16c4ebf049ed087caca083e2c
Jesse <jesse.l.heller@gmail.com>
2020-06-19 10:54:55 -0400

    Added view campgrounds

--- Capstone/java/src/main/java/com/techelevator/reservation/ReservationDAO.java
+++ Capstone/java/src/main/java/com/techelevator/reservation/ReservationDAO.java
@@ -9,9 +9,5 @@ public interface ReservationDAO {
     public Reservation createReservation(Reservation reservation);
     public Reservation searchForReservationByReservationId(Long resIdSearch);
     public Reservation createNewReservation(String name, Date startDate, Date endDate);
-<<<<<<< HEAD
-	
-=======
->>>>>>> 9010e58bd6b8d7d00d02d8b5154b84b3e08a576e
 
 }
